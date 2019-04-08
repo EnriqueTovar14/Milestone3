b372408244db92fb45fa1a650077807a3c65176c
enrique tovar <enriquektovar14@gmail.com>
2019-04-07 23:57:09 -0700

    commit

---
+++ Milestone3/InventoryManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone3
+{
+    class InvManager
+    {
+        //product list
+        private List<Product> items;
+
+        //constructer
+        public InvManager()
+        {
+            //initialize the attributes
+            items = new List<Product>();
+        }
+
+        public void addItem(Product item)
+        {   
+            items.Add(item);                  
+        }
+        public void removeItem(Product item)
+        {
+            items.Remove(item);
+        }
+        public Product findByIdOrName(string id, string name)
+        {
+            Product result = null;
+
+            for(int i = 0; i < items.Count(); i++)
+            {
+                //does this product have the same id as a param id?
+                if (items[i].prodId == id || items[i].name == name)
+                    result = items[i];
+            }
+            return result;
+        }
+
+        public void restockItem(Product item)
+        {
+            item.quantity = 500;
+        }
+        public void displayItems(InvManager manager)
+        {
+                Console.WriteLine(string.Join("\n", items));
+        }
+    }
+}
--- Milestone3/Program.cs
+++ Milestone3/Program.cs
@@ -10,6 +10,59 @@ namespace Milestone3
     {
         static void Main(string[] args)
         {
+            //test the invManager
+            InvManager manager = new InvManager();
+            //make a product to add
+            Product p = new Product();
+            p.name = "Silly Putty";
+            p.quantity = 143;
+            p.price = 2.29;
+            p.prodId = "123-sdfg";
+
+            //add p to the manager
+            manager.addItem(p);
+
+            Product c = new Product();
+            c.name = "Cards";
+            c.quantity = 140;
+            c.price = 2.25;
+            c.prodId = "124-sdfg";
+
+            manager.addItem(c);
+
+            manager.restockItem(c);
+
+            Console.WriteLine(c);
+
+            manager.displayItems(manager);
+
+            Product target = manager.findByIdOrName("123-sdfg","Silly Putty");
+            if(target == null)
+            {
+                System.Console.WriteLine("Product with id and name: " + target + " is not there");
+            }
+            else
+            {
+                System.Console.WriteLine(target);
+            }
+        }
+    }
+    class Product
+    {
+        public string name { set; get; }
+        public int quantity { set; get; }
+        public double price { set; get; }
+        public string prodId { set; get; }
+        public Product()
+        {
+            name = "";
+            quantity = 0;
+            price = 0;
+            prodId = "";
+        }
+        public override string ToString()
+        {
+            return "Name: " + name + ", Quantity: " + quantity + ", Price: $" + price + ",Id: " + prodId;
         }
     }
 }
